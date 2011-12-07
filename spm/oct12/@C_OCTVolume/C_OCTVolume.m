9d51919603cdb16c0a766310458a26aadc264071
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2011-12-07 13:41:04 +0000

    Corrections pour calculer la vitesse

--- spm/oct12/@C_OCTVolume/C_OCTVolume.m
+++ spm/oct12/@C_OCTVolume/C_OCTVolume.m
@@ -5,7 +5,7 @@ classdef C_OCTVolume < handle
    properties
       data = [];
       dims=[];
-      max_val = 1;
+      max_val = 0;
       min_val = 0;
    end
    methods
@@ -16,6 +16,8 @@ classdef C_OCTVolume < handle
       function saveint16(obj,name)
         Hstruct3D=fopen(name,'w');
         fwrite(Hstruct3D, obj.dims,'int16');
+        fwrite(Hstruct3D, obj.min_val,'double');
+        fwrite(Hstruct3D, obj.max_val,'double');
         fwrite(Hstruct3D, obj.data,'int16');
         fclose(Hstruct3D);
       end
@@ -23,6 +25,8 @@ classdef C_OCTVolume < handle
         Hstruct3D=fopen(name,'r');
         obj.dims=fread(Hstruct3D, 3,'int16');
         obj.dims=obj.dims';
+        obj.min_val = fread(Hstruct3D, obj.min_val,1,'double');
+        obj.max_val = fread(Hstruct3D, obj.max_val,1,'double');        
         obj.data=fread(Hstruct3D, inf,'int16');
         obj.data=reshape(obj.data,obj.dims);
         fclose(Hstruct3D);
